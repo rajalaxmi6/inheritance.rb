2098512da463368de929537b988304b3e1a2f5a1
Rajalaxmi Katkam <83289230+rajalaxmi6@users.noreply.github.com>
2021-05-13 19:32:52 +0530

    Create Human.rb

---
+++ Human.rb
@@ -0,0 +1,24 @@
+class Human
+	attr_accessor :occupation 
+	def initialize(occupation)
+		@occupation = occupation
+end
+
+
+
+class Student
+	attr_accessor :occupation
+	def initialize(occupation)
+
+		super(occupation)
+		puts "I am Student"
+		
+
+	def to_s
+		"#{self.class} @occupation : #{@occupation}"
+	end
+end
+
+
+occupation = occupation.new
+puts "I am Student" 
